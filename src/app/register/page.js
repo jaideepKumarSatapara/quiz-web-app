2d78492880e60a5328e55361159e9daf485ba3f2
jaideepKumarSatapara <jaideepkumar1407@gmail.com>
2025-02-11 16:08:12 +0530

    register page content added

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Register() {
 
     return (
-        <div>This is a Registration Page</div>
+        <>
+            <div>WELCOME ABOARD !!!</div>
+            <div>This is a Registration Page</div>
+        </>
     )
 }
