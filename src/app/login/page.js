d0d08c25a22c3ca485b8fea510e4356cff66144a
jaideepKumarSatapara <jaideepkumar1407@gmail.com>
2025-02-11 11:25:46 +0530

    Login Content Update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Login() {
 
     return (
-        <div>This is a login page and you can not move further without logging in!</div>
+        <>
+            <div>This is a login page and you can not move further without logging in!</div>
+            <div> Hellow Viewers!</div>
+        </>
     )
 }
